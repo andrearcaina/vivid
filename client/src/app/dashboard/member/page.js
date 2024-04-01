918dbe557be961565eadf6fff40399126bf963ae
heintzJ <96451646+heintzJ@users.noreply.github.com>
2024-04-01 18:10:50 -0400

    Made boxes look nicer

--- client/src/app/dashboard/member/page.js
+++ client/src/app/dashboard/member/page.js
@@ -1,32 +1,27 @@
 'use client';
 import { Welcome } from '@/components';
+import Announcements from '@/components/general/announcements';
 import { useRedirect } from '@/hooks/useRedirect';
 
 export default function MemberDashboard() {
     useRedirect('member');
     
     return (
-        <div className='bg-gray-100'>
+        <div className='min-h-screen bg-gray-100 bg-'>
             <h1 className='text-center text-3xl p-5'>Dashboard</h1>
             <div className='grid grid-cols-2'>
-                <div className='w-full rounded-md overflow-hidden shadow-md ml-2 mr-2'>
-                    <div className="bg-white p-4">
-                        <h2 className='underline'>Announcements</h2>
-                        <p>This is where announcements will go</p>
-                    </div>
+                <div className='w-full rounded-md overflow-hidden shadow-md ml-2 mr-2 bg-white p-4'>
+                    <h2 className='underline'>Announcements</h2>
+                    <Announcements></Announcements>
                 </div>
                 <div className='flex flex-col w-full'>
-                    <div className='rounded-md overflow-hidden shadow-md mb-4 ml-4 mr-2'>
-                        <div className="bg-white p-4">
-                            <h2 className='underline'>Calendar</h2>
-                            <p>Need to create a calendar</p>
-                        </div>
+                    <div className='rounded-md overflow-hidden shadow-md mb-4 ml-4 mr-2 bg-white p-4'>
+                        <h2 className='underline'>Calendar</h2>
+                        <p>Need to create a calendar</p>
                     </div>
-                    <div className='rounded-md overflow-hidden shadow-md ml-4 mr-2'>
-                        <div className="bg-white p-4">
-                            <h2 className='underline'>Finances</h2>
-                            <p>money stuffs</p>
-                        </div>
+                    <div className='rounded-md overflow-hidden shadow-md ml-4 mr-2 bg-white p-4'>
+                        <h2 className='underline'>Finances</h2>
+                        <p>money stuffs</p>
                     </div>
                 </div>
             </div>
