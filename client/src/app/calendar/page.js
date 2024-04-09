c828eab8d723cb072419fafd5152ba546b1d4d6f
Joseph Leung <leung.joseph@outlook.com>
2024-04-09 18:22:36 -0400

    created calendar

---
+++ client/src/app/calendar/page.js
@@ -0,0 +1,64 @@
+'use client';
+import { Calendar, momentLocalizer, } from 'react-big-calendar'
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useAuthContext } from '@/hooks/useAuthContext';
+import { useDarkMode } from '@/hooks/useDarkModeContext';
+import { UnAuthorized, Deactivated } from '@/components';
+import { fetchMemberEnrolledClasses } from '@/utils/classes';
+import { useState, useEffect } from 'react';
+
+export default function CalendarPage() {
+    const { authReady, role, activated } = useAuthContext();
+    const { darkMode } = useDarkMode();
+    // constants needed for calendar
+    const localizer = momentLocalizer(moment);
+
+    // fetching all user's course data
+    const [courses, setCourses] = useState();
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const data = await fetchMemberEnrolledClasses();
+                setCourses(data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchData();
+    }, []);
+
+    // function to convert the courses json data to event format
+    function dataToEvents() {
+        const newEvents = courses.class_name.map((name, i) => ({
+            title: name + ": Instructor (" + courses.instructor_name + ")",
+            start: new Date(courses.class_datetime[i]),
+            end: new Date(courses.class_datetime[i])
+        }));
+
+        return newEvents;
+    }
+
+    if (authReady && activated) {
+        return(
+            <div className={darkMode ? 'dark' : ''}>
+                <div className="calendarPage dark:bg-gray-700 dark:text-white h-[92vh] flex items-center justify-center py-10">
+                    <Calendar
+                    localizer={localizer} 
+                    events={courses && courses.class_name ? (
+                        dataToEvents()
+                    ) : []}
+                    startAccessor="start" 
+                    endAccessor="end"
+                    // defaultView='month'
+                    />
+                </div>
+            </div>
+            );
+    } else if (role == 'member' && !activated) {
+        return <Deactivated />;
+    } else {
+        return <UnAuthorized />;
+    }
+}
