5db950c0b6137bbdeffee8baa3e5445fce84a7a5
Joseph Leung <leung.joseph@outlook.com>
2024-04-09 22:52:32 -0400

    added new finance table to db

--- server/finance_tracking/models.py
+++ server/finance_tracking/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from user_auth.models import User
 
 # Create your models here.
+class finance_tracking(models.Model):
+    class Meta:
+        db_table = "finance_tracking"
+
+    user = models.OneToOneField(User, blank=True, null=True, on_delete=models.CASCADE)
+    payment_balance = models.IntegerField(default=0)
+    number_classes_taught = models.IntegerField(default=0)
